44802919027147e23de0539dafd9e38f1e7cd575
Thyrst <thyrst@seznam.cz>
2024-08-10 16:44:39 +0200

    add task data

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="vite/client" />
+
+interface Article {
+  author: string;
+  date: string;
+  text: string;
+}
+
+interface Comment {
+  id: number;
+  author: string;
+  text: string;
+  date: string;
+}
+
+interface Document {
+  __article: Article;
+  __comments: Comment[];
+  __moreComments: Comment[];
+}
